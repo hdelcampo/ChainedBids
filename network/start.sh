2e9f7a5cd64045c3f71efcd09fdf3841cf6f2ba2
FoxeyYy <hectorcampop@outlook.com>
2017-12-22 11:47:23 +0100

    Added CouchDB option

--- network/start.sh
+++ network/start.sh
@@ -3,5 +3,21 @@
 export PATH=${PWD}/../bin:${PWD}:$PATH
 export FABRIC_CFG_PATH=${PWD}
 
-CHANNEL_NAME=chainedbidschannel TIMEOUT=10 DELAY=3 docker-compose -f docker-compose.yaml up -d
+COUCHDB=false
+
+while getopts "c" opt; do
+  case "$opt" in
+    c)  COUCHDB=true
+    ;;
+  esac
+done
+
+if [ "${COUCHDB}" == false ]; then
+    echo "Starting using LevelDB..."
+    CHANNEL_NAME=chainedbidschannel TIMEOUT=10 DELAY=3 docker-compose -f docker-compose.yaml up -d
+else
+    echo "Starting using CouchDB..."
+    CHANNEL_NAME=chainedbidschannel TIMEOUT=10 DELAY=3 docker-compose -f docker-compose.yaml -f docker-compose-couch.yaml up -d
+fi
+
 docker logs -f cli
